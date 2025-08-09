6f75110fd6ebdc717cfa3ee005401eb21c7dc06b
Tolulope Malomo <malomo.tolz@gmail.com>
2025-08-09 17:33:16 +0100

    raffle wip

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,14 +1,64 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
 
-contract Counter {
-    uint256 public number;
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+/**
+ * @title Simple Raffle Contract
+ * @author Tolulope Malomo 
+ * @notice For creating a simple raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+contract Raffle {
+    // Errors 
+    error Raffle__SendMoreToEnterRaffle();
+
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players; //we make address payable, since we need to pay raffle price to them eventually
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
     }
 
-    function increment() public {
-        number++;
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
+        if(msg.value < i_entranceFee){
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+
+        //Why we emit events?
+        //1. Makes migration to a new contract easier.
+        //2. Makes front end "indexing" easier.
+    }
+
+    function pickWinner() public {
+
+    }
+
+    /**
+     * Getter Functions
+     */
+    function getEntranceFee() external view returns (uint256){
+        return i_entranceFee;
     }
 }
